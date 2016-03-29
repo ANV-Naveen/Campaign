b003a5f954ce69b89bd51d1b551ccb19cb1b99a0
root <root@av3.anvaya.com>
2016-03-29 17:27:57 +0530

    Commiting Ui Changes

---
+++ js/picker.js
@@ -0,0 +1,11 @@
+  $(document).ready(function () {
+               
+                $('#id1').datepicker({
+                    format: "dd/mm/yyyy"
+                }); 
+                $('#id2').datepicker({
+                    format: "dd/mm/yyyy"
+                });  
+ 
+
+            });
